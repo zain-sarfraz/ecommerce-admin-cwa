26a1c115d2432917608bec0bf2f557602989d3ed
Zain Sarfraz <133741478+zain-sarfraz@users.noreply.github.com>
2025-03-11 10:54:27 +0500

    Update image-upload.tsx

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -53,7 +53,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
           </div>
         ))}
       </div>
-      <CldUploadWidget onUpload={onUpload} uploadPreset="ophaffw1">
+      <CldUploadWidget onUpload={onUpload} uploadPreset="dmiehnoq9">
         {({ open }) => {
           const onClick = () => {
             open();
